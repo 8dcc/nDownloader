0836207f9fc6ad714f583cfcb22b2c43c7ae7db0
ravi0li <mikel.veg777@gmail.com>
2021-05-30 14:30:11 +0200

    v1.5
    Added ID checking

--- nCustom.py
+++ nCustom.py
@@ -1,7 +1,5 @@
 import os, requests, base64, random, string, sys, time
 from colorama import Fore, Style
-# from lxml.html import fromstring
-# from bs4 import BeautifulSoup
 
 ############ EDIT ME ############
 operative_system = "win"        #  << Put here win or lin
@@ -11,7 +9,7 @@ def banner():
     print("         _____                        __                 __             ")
     print(" .-----.|     \\.-----.--.--.--.-----.|  |.-----.---.-.--|  |.-----.----.")
     print(" |     ||  --  |  _  |  |  |  |     ||  ||  _  |  _  |  _  ||  -__|   _|")
-    print(" |__|__||_____/|_____|________|__|__||__||_____|___._|_____||_____|__|   Made by r4v10l1")
+    print(" |__|__||_____/|_____|________|__|__||__||_____|___._|_____||_____|__|  ")
 
 try:
     if operative_system == "win"    :
@@ -19,16 +17,17 @@ try:
         banner()
         print()
 
-    elif operative_system == "lin":  # Same as before but with colorama.
+    elif operative_system == lin:
         print(Style.RESET_ALL, Style.BRIGHT, Fore.WHITE)
         banner()
         print(Style.RESET_ALL, Fore.MAGENTA)
         print(Style.RESET_ALL)
     else:
-        print(" [!] Error. Invalid operative system, exiting...")  # If operative_system is not win or lin
+        print(" [!] Error. Invalid operative system, exiting...")
         exit(1)
-    print(" Starting at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))
-except KeyboardInterrupt:  # Detects if user ctrl + c while checking the os
+    print("     Starting at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))
+    print()
+except KeyboardInterrupt:
     print()
     print(" Detected Ctrl+C. Shutting down...")
     print()
@@ -36,40 +35,54 @@ except KeyboardInterrupt:  # Detects if user ctrl + c while checking the os
 
 try:
     if operative_system == "win":
-        pass  # I'm too lazy to check if the folder exists
+        pass
     elif operative_system == "lin":
-        os.system("rm -r nDownloads")  # Remove the folder
-        os.system("mkdir nDownloads")  # Make the folder
-    print()
-    nID = input(" [?] Write here your image ID: ")  # The image ID, not the sauce. See README.md to see what I mean
-    pageNumber = 1  # Start by trying to download page 1 before the loop adds more pages
+        os.system("rm -r nDownloads")
+        os.system("mkdir nDownloads")
+    else:
+        print()
+        print(" [!] Error. Invalid operative system, exiting...")
+        print()
+    nID = input(" [?] Write here your image ID: ")
+    if len(nID) != 7:
+        print()
+        print(" [!] Invalid ID. Length must be 7 . Exiting...")
+        print()
+        exit(1)
+    pageNumber = 1
     while True:
-        nURL = "https://i.nhentai.net/galleries/" + nID + "/" + str(pageNumber) + ".jpg"  # Base URL + ID + number.jpg
+        nURL = "https://i.nhentai.net/galleries/" + nID + "/" + str(pageNumber) + ".jpg"
         try:
             r = requests.get(nURL, allow_redirects=True)
-            images = {"image/jpeg"}  # Check if the request gives a image (AKA the id is correct and there are no erros)
-            if r.headers["content-type"] not in images:  # Check if we did not get an image from the request
-                print()
-                print(" [+] Done downloading. Exiting...")  # Once we dont get more images, the download is done and we can exit
-                print(" Stopped at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))  # Display when the program stopped
-                exit(1)
-            elif r.headers["content-type"] in images:  # If we got an image from the request
-                os.system("echo.> nDownloads/" + str(nID) + "_" + str(pageNumber) + ".jpg")  # ID + _ + number.jpg  -->  1233212_5.jpg
-                PATH = "nDownloads/" + str(nID) + "_" + str(pageNumber) + ".jpg"  # Same as above
-                open(str(PATH), "wb").write(r.content)  # Write the contents on the files
-                if pageNumber == 1:  # If the page we are downloading is the first one (AKA this will execute once per id)
+            images = {"image/jpeg"}
+            if r.headers["content-type"] not in images:
+                if pageNumber == 1:
                     print()
-                    print(" [i] Request for " + str(nID) + " is good. Downloading in " + PATH)  # Display that all went good
-                pageNumber += 1  # Adds a page number which will be downloaded in the loop
-        except Exception as e:  # If there is an error
-            print(" [!] An error ocurred: %s" % (e))
-            print(" Stopped at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))  # Display when the program stopped
+                    print(" [!] Error. ID not found. Exiting...")
+                    print()
+                    exit(1)
+                else:
+                    print()
+                    print(" [+] Done downloading. Exiting...")
+                    print()
+                    exit(1)
+            elif r.headers["content-type"] in images:
+                os.system("echo.> nDownloads/" + str(nID) + "_" + str(pageNumber) + ".jpg")
+                PATH = "nDownloads/" + str(nID) + "_" + str(pageNumber) + ".jpg"
+                open(str(PATH), "wb").write(r.content)
+                if pageNumber == 1:
+                    print()
+                    print(" [i] Request for " + str(nID) + " is good. Downloading in " + PATH)
+                pageNumber += 1
+        except Exception as e:
+            print(" %s%s[!] An error ocurred: %s%s" % (Style.BRIGHT, Fore.RED, Style.RESET_ALL, e))
+            print(" Stopped at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))
             print()
             exit(1)
-except KeyboardInterrupt:  # If the user ctrl + c during the loop / download
+except KeyboardInterrupt:
     print()
     print(" Detected Ctrl+C. Shutting down...")
-    print(" Stopped at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))  # Display when the program stopped
+    print(" Stopped at "+ time.strftime("%d %b %Y - %H:%M:%S", time.gmtime()))
     print()
     exit(1)
 
